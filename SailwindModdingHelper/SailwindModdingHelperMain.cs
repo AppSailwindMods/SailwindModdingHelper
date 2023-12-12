ab40f2732461a1d9e26519b0e6637c5d596d0ac4
Ricardo <rmmx03@gmail.com>
2023-12-12 13:19:02 +0000

    Renamed Main to SailwindModdingHelperMain

--- SailwindModdingHelper/SailwindModdingHelperMain.cs
+++ SailwindModdingHelper/SailwindModdingHelperMain.cs
@@ -1,20 +1,12 @@
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using UnityEngine;
 
 namespace SailwindModdingHelper
 {
     [BepInPlugin(GUID, NAME, VERSION)]
-    public class Main : BaseUnityPlugin
+    public class SailwindModdingHelperMain : BaseUnityPlugin
     {
         public const string GUID = "com.app24.sailwindmoddinghelper";
         public const string NAME = "Sailwind Modding Helper";
